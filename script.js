5bb66476a79e55dd68fd74af6e6a4764559b1dde
Vaibhavi <139929422+NIKHILstotic@users.noreply.github.com>
2025-09-07 10:20:40 +0000

    i did it

---
+++ script.js
@@ -0,0 +1,10 @@
+let sel = document.querySelector("select");
+sel.addEventListener("change",function (dets) {
+let devise = document.querySelector("#devise")
+devise.textContent = "devise okokk";
+console.log(devise);
+
+
+})
+
+     
